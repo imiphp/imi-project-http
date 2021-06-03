97ceeb7b1738887dc2c87897d96be2a1a3a43164
Yurun <admin@yurunsoft.com>
2021-06-03 13:34:02 +0800

    修改 host=0.0.0.0

--- config/config.php
+++ config/config.php
@@ -22,7 +22,7 @@ return [
     'mainServer'    =>    [
         'namespace'    =>    'ImiApp\ApiServer',
         'type'        =>    Imi\Server\Type::HTTP,
-        'host'        =>    '127.0.0.1',
+        'host'        =>    '0.0.0.0',
         'port'        =>    8080,
         'configs'    =>    [
             // 'worker_num'        =>  8,
@@ -35,7 +35,7 @@ return [
         // 'SubServerName'   =>  [
         //     'namespace'    =>    'ImiApp\XXXServer',
         //     'type'        =>    Imi\Server\Type::HTTP,
-        //     'host'        =>    '127.0.0.1',
+        //     'host'        =>    '0.0.0.0',
         //     'port'        =>    13005,
         // ]
     ],
