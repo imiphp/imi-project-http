cb8f89f59749f41c5af882b471c60cdef011f791
Yurun <admin@yurunsoft.com>
2021-09-16 09:46:27 +0800

    修改连接池最小连接数为0

--- config/config.php
+++ config/config.php
@@ -67,7 +67,7 @@ return [
                 'class'        => \Imi\Swoole\Db\Pool\CoroutineDbPool::class,
                 'config'       => [
                     'maxResources'    => 10,
-                    'minResources'    => 1,
+                    'minResources'    => 0,
                 ],
             ],
             'resource'    => [
@@ -84,7 +84,7 @@ return [
                 'class'        => \Imi\Swoole\Redis\Pool\CoroutineRedisPool::class,
                 'config'       => [
                     'maxResources'    => 10,
-                    'minResources'    => 1,
+                    'minResources'    => 0,
                 ],
             ],
             'resource'    => [
